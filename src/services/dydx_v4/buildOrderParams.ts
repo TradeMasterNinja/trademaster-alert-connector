68220aca874e26515fdee346686bd570b7a56697
TradeMasterNinja <152562756+TradeMasterNinja@users.noreply.github.com>
2024-05-05 13:29:43 -0700

    allow for market close current position

--- src/services/dydx_v4/buildOrderParams.ts
+++ src/services/dydx_v4/buildOrderParams.ts
@@ -14,57 +14,72 @@ export const dydxV4BuildOrderParams = async (alertMessage: AlertObject) => {
             throw new Error('Failed to fetch account data.');
         }
 
-        const orderSide = alertMessage.order === 'buy' ? OrderSide.BUY : OrderSide.SELL;
-        const positionSide = alertMessage.position === 'long' ? 'LONG' : 'SHORT';
-		const reduceOrder = (orderSide===OrderSide.BUY && positionSide==="SHORT") || (orderSide===OrderSide.SELL && positionSide==="LONG");
+        const positionSide = alertMessage.position === 'long' ? 'LONG' : alertMessage.position === 'short' ? 'SHORT' : 'FLAT';
 		const { account, openPositions } = accountData;    
         const currentPosition = openPositions[market];
+        const orderSide = alertMessage.order === 'buy' ? OrderSide.BUY : OrderSide.SELL;
+        const reduceOrder = (orderSide===OrderSide.BUY && positionSide==="SHORT") || (orderSide===OrderSide.SELL && positionSide==="LONG");
+		
 		//check if user already manually closed position
 		if (!currentPosition && reduceOrder) {
 			throw new Error('cannot reduce '+market+' order. Position has already been closed or does not exist');
 		}
-        const currentPositionSize = currentPosition ? Math.abs(Number(currentPosition.size)) : 0;
-		const latestPrice = Number(alertMessage.price);
-        const alertSize = alertMessage.sizeByLeverage ? Number(alertMessage.sizeByLeverage) : alertMessage.sizeUsd ? Number(alertMessage.sizeUsd) : alertMessage.size ? Number(alertMessage.size) : 0;
-		console.log("alertSize", alertSize);
-		let orderSize: number = (alertMessage.reverse && currentPosition.side !== positionSide) ? currentPositionSize : 0;
+        if (!currentPosition && positionSide === "FLAT") {
+			throw new Error('cannot close '+market+' order. Position has already been closed or does not exist');
+		}
 
-        // populate orderSize full current position size if alert side is in opposite direction of current open position
-        if (currentPosition && currentPosition.side === "LONG" && orderSide === OrderSide.SELL && !reduceOrder) {
-            orderSize = currentPositionSize;
+        // calculate current positionSize
+        const currentPositionSize = currentPosition ? Math.abs(Number(currentPosition.size)) : 0;
+        const alertSize = alertMessage.sizeByLeverage ? Number(alertMessage.sizeByLeverage) : alertMessage.sizeUsd ? Number(alertMessage.sizeUsd) : alertMessage.size ? Number(alertMessage.size) : 0;
+		const latestPrice = Number(alertMessage.price)
+        const newOrder = {
+            orderSize:Number((alertMessage.reverse && currentPosition.side !== positionSide) ? currentPositionSize : 0),
+            orderSide:orderSide
         }
-        if (currentPosition && currentPosition.side === "SHORT" && orderSide === OrderSide.BUY && !reduceOrder) {
-            orderSize = currentPositionSize;
-        }
-        
-        if (alertMessage.sizeByLeverage) {
-			if (reduceOrder) {
-                const sizeUsd = ((currentPositionSize*latestPrice) * alertSize) / latestPrice;
-                orderSize = sizeUsd >= currentPositionSize ? currentPositionSize : sizeUsd
-			} else {
-				orderSize += (account.equity * alertSize) / latestPrice;
-			}
-        } else if (alertMessage.sizeUsd) {
-			if (reduceOrder) {
-				const sizeUsd = alertSize / latestPrice;
-				orderSize = sizeUsd >= currentPositionSize ? currentPositionSize : sizeUsd
-			} else {
-				orderSize += alertSize / latestPrice;
-			}
-            
-        }  else {
-            if (reduceOrder) {
-                orderSize = alertSize >= currentPositionSize ? currentPositionSize : alertSize
-            } else {
-                orderSize += alertSize;
+
+        // calculate new orderSize
+        if (positionSide === "FLAT") {
+            // market close current order
+            newOrder.orderSize = currentPositionSize;
+            newOrder.orderSide = currentPosition.side === "LONG" ? OrderSide.SELL : OrderSide.BUY;
+        } else {
+            // use full current position size if alert side is in opposite direction of current open position
+            if (currentPosition && currentPosition.side === "LONG" && orderSide === OrderSide.SELL && !reduceOrder) {
+                newOrder.orderSize = currentPositionSize;
+            }
+            if (currentPosition && currentPosition.side === "SHORT" && orderSide === OrderSide.BUY && !reduceOrder) {
+                newOrder.orderSize = currentPositionSize;
             }
             
+            if (alertMessage.sizeByLeverage) {
+                if (reduceOrder) {
+                    const sizeUsd = ((currentPositionSize*latestPrice) * alertSize) / latestPrice;
+                    newOrder.orderSize = sizeUsd >= currentPositionSize ? currentPositionSize : sizeUsd
+                } else {
+                    newOrder.orderSize += (account.equity * alertSize) / latestPrice;
+                }
+            } else if (alertMessage.sizeUsd) {
+                if (reduceOrder) {
+                    const sizeUsd = alertSize / latestPrice;
+                    newOrder.orderSize = sizeUsd >= currentPositionSize ? currentPositionSize : sizeUsd
+                } else {
+                    newOrder.orderSize += alertSize / latestPrice;
+                }
+                
+            }  else {
+                if (reduceOrder) {
+                    newOrder.orderSize = alertSize >= currentPositionSize ? currentPositionSize : alertSize
+                } else {
+                    newOrder.orderSize += alertSize;
+                }
+                
+            }
         }
-
+             
         const orderParams: dydxV4OrderParams = {
             market,
-            side: orderSide,
-            size: Number(orderSize),
+            side: newOrder.orderSide,
+            size: Number(newOrder.orderSize),
             price: Number(latestPrice)
         };
 
